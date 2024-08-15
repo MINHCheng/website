319e47e55b7ff2aa5b407e5766311cae3291ea8a
MINHCheng <119878298+MINHCheng@users.noreply.github.com>
2024-08-15 16:38:06 -0400

    home

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,6 +11,8 @@ const Home = () => {
         const summary = document.querySelectorAll('.Summary');
         const letters = document.querySelectorAll('.letters');
         const spaceShuttle = document.querySelector('.fa-rocket');
+        const shootingStar = document.querySelectorAll('.shooting');
+        const laptop = document.querySelectorAll('.laptop');
 
         const t1 = gsap.timeline();
 
@@ -18,7 +20,8 @@ const Home = () => {
             x: 500,
             duration: 2,
             stagger: 0.5,
-            opacity: 0
+            opacity: 0,
+            delay: 3,
         })
             .to(letters, {
                 color: '#17BEBB', // Target color
@@ -29,25 +32,42 @@ const Home = () => {
                 },
                 ease: 'power1.inOut',
                 repeat: -1,
-                yoyo: true
-            });
+                yoyo: true,
+
+            })
 
         // Create and adjust the motion path interactively
         gsap.to(spaceShuttle, {
-            duration: 5,
+            duration: 2,
             opacity: 1,
             color: '#f5f5f5',
             motionPath: {
                 path: [
-                    { x: 500, y: 0 },        // Start off the right side of the screen
-                    { x: -150, y: -200 },    // Move up and to the left
-                    { x: -50, y: -250 }      // End just above the words in the middle
+                    { x: 300, y: -100 },    // Start off-screen to the right
+                    { x: 600, y: -100 },   // Move up and right
+                    { x: 1100, y: 150 }   // End just above the words in the middle
                 ],
                 end: .75,
                 curviness: 1.25,
                 autoRotate: true
             },
+            delay: 1,
             ease: "power1.inOut",
+        });
+        gsap.from(shootingStar, {
+            duration: 1,
+            opacity: 0,
+            x: -500,
+            y: 100,
+            ease: 'power1.inOut',
+            stagger: 0.5,
+
+        });
+        gsap.from(laptop, {
+            duration: 3,
+            opacity: 0,
+            delay: 2,
+            ease: 'power1.inOut'
         });
 
     }, []);
@@ -55,37 +75,36 @@ const Home = () => {
     return (
         <>
             <div className="Homepage">
-                <Container className="Homepage">
+                <Container className="">
                     {/* Space shuttle icon */}
-                    <i className="fa-solid fa-rocket fa-rotate-270 fa-2xl" style={{ opacity: '0' }}></i>
-
-                    <div className="Summary big">
-                        HELLO WORLD!
-                    </div>
-                    <b>
+                    <i className="fa-solid fa-rocket fa-rotate-90 fa-2xl" style={{ opacity: '0' }}></i>
+                    <div className="summary-laptop">
                         <div className="Summary big">
-                            I'M <span className="letters">M</span><span className="letters">I</span><span className="letters">N</span><span className="letters">H,</span>
+                            HELLO WORLD!
                         </div>
-                    </b>
-                    <div className="Summary small">a second year <span style={{color:'#17BEBB'}}>computer engineering</span> student</div>
-                    <div className="Summary small">at the University of <span style={{color:'#17BEBB'}}>Waterloo.</span></div>
+                        <b>
+                            <div className="Summary big">
+                                I'M <span className="letters">M</span><span className="letters">I</span><span className="letters">N</span><span className="letters">H,</span>
+                            </div>
+                        </b>
+                        <div className="Summary small">a second year <span style={{ color: '#17BEBB' }}>computer engineering</span> student</div>
+                        <div className="Summary small">at the University of <span style={{ color: '#17BEBB' }}>Waterloo.</span></div>
+                        <i class="fa-solid fa-laptop-code laptop" ></i>
+                    </div>
 
                     {/* Star icon */}
-                    <i className="fa-solid fa-splotch star1"></i>
-                    <div className="bubble"></div>
-                    <i className="fa-solid fa-splotch star2"></i>
-                    <i class="fa-solid fa-laptop-code"></i>
+
+                    <i className="fa-solid fa-splotch shooting star3"></i>
+                    <i className="fa-solid fa-splotch shooting star4"></i>
+                    <i className="fa-solid fa-splotch shooting star5"></i>
+
                 </Container>
             </div>
-
-            <div className="Skills">
-                <Container className="Skills Content">
-                    <Row xs={1} md={2} lg={3} className="g-3">
-                        {/* Add your Card components here */}
-                    </Row>
-                </Container>
+            <i className="fa-solid fa-splotch star1"></i>
+            <i className="fa-solid fa-splotch star2"></i>
+            <div className="Homepage">
+                <div className="bubble"></div>
             </div>
-
         </>
     )
 }
